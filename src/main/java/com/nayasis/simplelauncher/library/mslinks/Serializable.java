f752e0636a0f1a20657038424dffe4710983dd37
nayasis <naysis@gmail.com>
2019-03-28 19:48:46 +0900

    move library to maven reference.

--- src/main/java/com/nayasis/simplelauncher/library/mslinks/Serializable.java
+++
@@ -1,9 +0,0 @@
-package com.nayasis.simplelauncher.library.mslinks;
-
-import com.nayasis.simplelauncher.library.mslinks.io.ByteWriter;
-
-import java.io.IOException;
-
-public interface Serializable {
-	void serialize( ByteWriter bw ) throws IOException;
-}
