1221caf1a8105743f462a7393d663a12d6a75ce9
nayasis <nayasis@gmail.com>
2020-11-07 13:39:00 +0900

    hello world 구동

--- src/main/kotlin/com/github/nayasis/helloworld/HelloWorldApp.kt
+++ src/main/kotlin/com/github/nayasis/helloworld/HelloWorldApp.kt
@@ -1,17 +1,19 @@
 package com.github.nayasis.helloworld
 
 import javafx.scene.text.FontWeight
-import tornadofx.App
-import tornadofx.Stylesheet
-import tornadofx.c
-import tornadofx.px
+import tornadofx.*
 
 class HelloWorldApp : App( HelloWorld::class, Styles::class )
+
+fun main( args:Array<String> ) {
+    launch<HelloWorldApp>( args )
+}
 
 class Styles : Stylesheet() {
     init {
         label {
             fontSize = 20.px
+            padding = box( 5.px )
             fontWeight = FontWeight.BOLD
             backgroundColor += c("#cecece")
         }
--- src/main/kotlin/com/github/nayasis/simplelauncher/core/javafx/SpringFxmlLoader.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/core/javafx/SpringFxmlLoader.kt
@@ -3,26 +3,26 @@ package com.github.nayasis.simplelauncher.core.javafx
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.util.Callback
-import org.springframework.context.ApplicationContext
-import org.springframework.stereotype.Component
+//import org.springframework.context.ApplicationContext
+//import org.springframework.stereotype.Component
 import java.util.*
 
-@Component
-class SpringFxmlLoader(
-        private val resourceBundle:ResourceBundle,
-        private val context:ApplicationContext,
-) {
-
-    fun load( fxmlPath:String ):Parent {
-
-        val loader = FXMLLoader();
-
-        loader.controllerFactory = Callback<Class<*>, Any> { context.getBean(it) }
-        loader.resources = resourceBundle
-        loader.location = javaClass.getResource( fxmlPath )
-
-        return loader.load()
-
-    }
-
-}
+//@Component
+//class SpringFxmlLoader(
+////        private val resourceBundle:ResourceBundle,
+//        private val context:ApplicationContext,
+//) {
+//
+//    fun load( fxmlPath:String ):Parent {
+//
+//        val loader = FXMLLoader()
+//
+//        loader.controllerFactory = Callback<Class<*>, Any> { context.getBean(it) }
+////        loader.resources = resourceBundle
+//        loader.location = javaClass.getResource( fxmlPath )
+//
+//        return loader.load()
+//
+//    }
+//
+//}
