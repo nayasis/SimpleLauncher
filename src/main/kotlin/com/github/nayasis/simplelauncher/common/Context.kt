8a4353029c3347af96e21f1a58d6363b20415094
nayasis <nayasis@gmail.com>
2021-07-23 20:43:03 +0900

    add view injection

--- src/main/kotlin/com/github/nayasis/simplelauncher/common/Context.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/common/Context.kt
@@ -1,15 +1,18 @@
 package com.github.nayasis.simplelauncher.common
 
 import com.github.nayasis.simplelauncher.service.ConfigService
+import com.github.nayasis.simplelauncher.service.LinkService
+import com.github.nayasis.simplelauncher.view.Main
 import tornadofx.FX
+import tornadofx.find
 import kotlin.reflect.KClass
 
 class Context { companion object {
 
-    fun <T:Any> bean(klass: KClass<T>): T {
-        return FX.dicontainer!!.getInstance(klass)
-    }
+    fun <T:Any> bean(klass: KClass<T>): T = FX.dicontainer!!.getInstance(klass)
 
-    val configService: ConfigService by lazy { bean(ConfigService::class)}
+    val configService by lazy { bean(ConfigService::class)}
+    val linkService   by lazy { bean(LinkService::class)}
+    val main          by lazy { find(Main::class) }
 
 }}
