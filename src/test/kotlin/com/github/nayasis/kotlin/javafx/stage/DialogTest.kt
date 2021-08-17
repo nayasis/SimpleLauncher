b498a6a985ae8fe3c87199f6b3331b24e93fb53b
nayasis <nayasis@gmail.com>
2021-08-17 15:47:16 +0900

    테스트 케이스 수정

--- src/test/kotlin/com/github/nayasis/kotlin/javafx/stage/DialogTest.kt
+++ src/test/kotlin/com/github/nayasis/kotlin/javafx/stage/DialogTest.kt
@@ -1,29 +1,35 @@
 package com.github.nayasis.kotlin.javafx.stage
 
+import javafx.application.Application
+import javafx.scene.text.Font
+import mu.KotlinLogging
 import tornadofx.App
+import tornadofx.Stylesheet
 import tornadofx.View
 import tornadofx.action
 import tornadofx.button
 import tornadofx.launch
 import tornadofx.vbox
 
+private val logger = KotlinLogging.logger {}
+
 fun main(args: Array<String>) {
     launch<DialogTest>(args)
 }
 
-class DialogTest: App(DialogTestView::class)
+class DialogTest: App(DialogTestView::class,MyStylesheet::class)
 
 class DialogTestView: View("dialog test") {
 
     override val root = vbox {
         button("error") {
            action {
-               Dialog.error("test error", RuntimeException("test"))
+               Dialog.error("test error (1234567890)", RuntimeException("test"))
            }
         }
         button("confirm") {
             action {
-                if( Dialog.confirm("is it ok ??") ) {
+                if( Dialog.confirm("is it ok (1234567890) ??") ) {
                     Dialog.alert("it is ok !!")
                 }
             }
@@ -32,4 +38,23 @@ class DialogTestView: View("dialog test") {
         prefHeight = 200.0
     }
 
+    init {
+        val userAgentStylesheet = Application.getUserAgentStylesheet()
+
+        logger.debug {
+            "style sheet : ${Application.getUserAgentStylesheet()}"
+            "font : ${Font.getDefault().name}"
+        }
+
+    }
+
+}
+
+// set default font
+class MyStylesheet : Stylesheet() {
+    init {
+        root {
+            fontFamily = "Arial"
+        }
+    }
 }
