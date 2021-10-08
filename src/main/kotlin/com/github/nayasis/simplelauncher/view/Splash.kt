16b7249376ada02f78c739992fe33681fb8e6d7c
nayasis <nayasis@gmail.com>
2021-10-08 18:13:55 +0900

    add move handler to Splash screen

--- src/main/kotlin/com/github/nayasis/simplelauncher/view/Splash.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/view/Splash.kt
@@ -3,6 +3,7 @@ package com.github.nayasis.simplelauncher.view
 import com.github.nayasis.kotlin.javafx.preloader.CloseNotificator
 import com.github.nayasis.kotlin.javafx.preloader.NPreloader
 import com.github.nayasis.kotlin.javafx.preloader.ProgressNotificator
+import com.github.nayasis.kotlin.javafx.stage.addMoveHandler
 import com.github.nayasis.kotlin.javafx.stage.loadDefaultIcon
 import com.github.nayasis.kotlin.tornadofx.extension.toScene
 import javafx.application.Platform
@@ -29,6 +30,7 @@ class Splash: NPreloader() {
             scene = view.toScene("/view/splash/splash.css")
             isAlwaysOnTop = true
             loadDefaultIcon()
+            addMoveHandler(view.root)
             show()
         }
     }
