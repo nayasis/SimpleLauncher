98f92d2dc6ac7a9d19bf1935021a446c8d20fd76
nayasis <nayasis@gmail.com>
2021-10-22 18:03:16 +0900

    simplify equals and hashcode

--- src/main/kotlin/com/github/nayasis/simplelauncher/view/terminal/TerminalConfig.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/view/terminal/TerminalConfig.kt
@@ -1,8 +1,11 @@
 package com.github.nayasis.simplelauncher.view.terminal
 
+import au.com.console.kassava.kotlinEquals
+import au.com.console.kassava.kotlinHashCode
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import kotlin.reflect.full.memberProperties
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 class TerminalConfig {
@@ -66,55 +69,8 @@ class TerminalConfig {
     @JsonIgnore
     var unixTerminalStarter = "/bin/bash -i"
 
-    override fun equals(o: Any?): Boolean {
-        if (this === o) return true
-        if (o == null || javaClass != o.javaClass) return false
-        val that = o as TerminalConfig
-        if (useDefaultWindowCopy != o.useDefaultWindowCopy) return false
-        if (clearSelectionAfterCopy != o.clearSelectionAfterCopy) return false
-        if (copyOnSelect != o.copyOnSelect) return false
-        if (ctrlCCopy != o.ctrlCCopy) return false
-        if (ctrlVPaste != o.ctrlVPaste) return false
-        if (fontSize != o.fontSize) return false
-        if (cursorBlink != o.cursorBlink) return false
-        if (scrollbarVisible != o.scrollbarVisible) return false
-        if (enableClipboardNotice != o.enableClipboardNotice) return false
-        if (java.lang.Double.compare(o.scrollWhellMoveMultiplier, scrollWhellMoveMultiplier) != 0) return false
-        if (sendEncoding != o.sendEncoding) return false
-        if (receiveEncoding != o.receiveEncoding) return false
-        if (cursorColor != o.cursorColor) return false
-        if (backgroundColor != o.backgroundColor) return false
-        if (foregroundColor != o.foregroundColor) return false
-        if (fontFamily != o.fontFamily) return false
-        if (userCss != o.userCss) return false
-        if (windowsTerminalStarter != o.windowsTerminalStarter) return false
-        return unixTerminalStarter == o.unixTerminalStarter
-    }
+    override fun equals(other: Any?) = kotlinEquals(other,TerminalConfig::class.memberProperties.toTypedArray())
 
-    override fun hashCode(): Int {
-        var result: Int
-        val temp: Long
-        result = sendEncoding.hashCode()
-        result = 31 * result + receiveEncoding.hashCode()
-        result = 31 * result + if (useDefaultWindowCopy) 1 else 0
-        result = 31 * result + if (clearSelectionAfterCopy) 1 else 0
-        result = 31 * result + if (copyOnSelect) 1 else 0
-        result = 31 * result + if (ctrlCCopy) 1 else 0
-        result = 31 * result + if (ctrlVPaste) 1 else 0
-        result = 31 * result + cursorColor.hashCode()
-        result = 31 * result + backgroundColor.hashCode()
-        result = 31 * result + fontSize
-        result = 31 * result + foregroundColor.hashCode()
-        result = 31 * result + if (cursorBlink) 1 else 0
-        result = 31 * result + if (scrollbarVisible) 1 else 0
-        result = 31 * result + if (enableClipboardNotice) 1 else 0
-        temp = java.lang.Double.doubleToLongBits(scrollWhellMoveMultiplier)
-        result = 31 * result + (temp xor (temp ushr 32)).toInt()
-        result = 31 * result + fontFamily.hashCode()
-        result = 31 * result + userCss.hashCode()
-        result = 31 * result + windowsTerminalStarter.hashCode()
-        result = 31 * result + unixTerminalStarter.hashCode()
-        return result
-    }
+    override fun hashCode(): Int = kotlinHashCode(properties = TerminalConfig::class.memberProperties.toTypedArray())
 
 }
