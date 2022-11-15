1bd4d7b2646387a1b2b46b1ae0da046b3de0e771
nayasis <nayasis@gmail.com>
2022-11-16 04:37:50 +0900

    add locale configuration

--- src/main/kotlin/com/github/nayasis/simplelauncher/Simplelauncher.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/Simplelauncher.kt
@@ -1,24 +1,30 @@
 package com.github.nayasis.simplelauncher
 
-import com.github.nayasis.kotlin.basica.model.Messages
 import com.github.nayasis.kotlin.basica.net.Networks
 import com.github.nayasis.kotlin.javafx.spring.SpringFxApp
 import com.github.nayasis.simplelauncher.common.BootLogger
 import com.github.nayasis.simplelauncher.view.Main
 import com.github.nayasis.simplelauncher.view.Splash
+import mu.KotlinLogging
 import org.apache.commons.cli.CommandLine
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.ApplicationContextInitializer
-import org.springframework.context.MessageSourceResolvable
 import tornadofx.launch
 import java.util.*
+
+private val logger = KotlinLogging.logger {}
 
 @SpringBootApplication
 class Simplelauncher: SpringFxApp(Main::class) {
 
+    @Value("\${simplalauncher.locale}")
+    var locale: String = ""
+
     private var bootLogger: BootLogger? = BootLogger()
 
     override fun onStart(command: CommandLine) {
+        Locale.setDefault(Locale.forLanguageTag(locale))
         bootLogger?.close()
         bootLogger = null
     }
@@ -31,7 +37,6 @@ class Simplelauncher: SpringFxApp(Main::class) {
 
 fun main(args: Array<String>) {
     Networks.trustAllCerts()
-//    Locale.setDefault(Locale.ENGLISH)
     SpringFxApp.run{
         loadMessage("/message/**.prop")
         loadDefaultIcon("/image/icon/favicon.png")
