cd97e3749407948ac311d596dd07638ae0149c79
nayasis <nayasis@gmail.com>
2022-12-09 00:43:19 +0900

    test boosting startup

--- src/main/kotlin/com/github/nayasis/simplelauncher/Simplelauncher.kt
+++ src/main/kotlin/com/github/nayasis/simplelauncher/Simplelauncher.kt
@@ -9,13 +9,42 @@ import mu.KotlinLogging
 import org.apache.commons.cli.CommandLine
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration
+import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration
+import org.springframework.boot.autoconfigure.dao.PersistenceExceptionTranslationAutoConfiguration
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration
+import org.springframework.boot.autoconfigure.http.HttpMessageConvertersAutoConfiguration
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration
+import org.springframework.boot.autoconfigure.transaction.jta.JtaAutoConfiguration
 import org.springframework.context.ApplicationContextInitializer
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import tornadofx.launch
 import java.util.*
 
 private val logger = KotlinLogging.logger {}
 
 @SpringBootApplication
+//@Configuration
+//@Import(value=[
+//    AopAutoConfiguration::class,
+//    DataSourceAutoConfiguration::class,
+//    HibernateJpaAutoConfiguration::class,
+//    JdbcTemplateAutoConfiguration::class,
+//    JpaRepositoriesAutoConfiguration::class,
+//    JtaAutoConfiguration::class,
+//    JacksonAutoConfiguration::class,
+//    PersistenceExceptionTranslationAutoConfiguration::class,
+//    PropertyPlaceholderAutoConfiguration::class,
+//    SqlInitializationAutoConfiguration::class,
+//    PersistenceExceptionTranslationAutoConfiguration::class,
+//    TransactionAutoConfiguration::class,
+//])
 class Simplelauncher: SpringFxApp(Main::class) {
 
     @Value("\${simplalauncher.locale:}")
