dce5e20854267ff693ccb92f4a11748e9e5eb8b4
nayasis <nayasis@gmail.com>
2025-09-22 20:16:52 +0900

    change test program to print first arg in console

--- src/test/resources/test-program/test.c
+++ src/test/resources/test-program/test.c
@@ -5,18 +5,19 @@
 #include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    int count = 10;
-    if (argc > 1) {
-        count = atoi(argv[1]);
-        if (count <= 0) count = 10;
-    }
 
     // Switch console to Unicode (UTF-16): use wprintf
     _setmode(_fileno(stdout), _O_U16TEXT);
 
-    for (int i = 0; i < count; i++) {
+    if (argc == 0) {
+        wprintf(L">>> no title\n");
+    } else if (argc > 1) {
+        wprintf(L">>> %hs\n", argv[1]);
+    }
+
+    for (int i = 0; i < 5; i++) {
         Sleep(1000);
-        wprintf(L"%d초 경과\n", i + 1);
+        wprintf(L"%d seconds elapsed\n", i + 1);
     }
     return 0;
 }
